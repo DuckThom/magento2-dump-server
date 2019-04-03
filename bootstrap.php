e4ba1ebd1dc4e088a0fd0979e3f95d7e7ad37b8f
Thomas Wiringa <t.wiringa@youwe.nl>
2019-04-03 15:00:29 +0200

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\Dumper\ServerDumper;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
+
+$cloner = new VarCloner();
+$fallbackDumper = \in_array(\PHP_SAPI, ['cli', 'phpdbg']) ? new CliDumper() : new HtmlDumper();
+$dumper = new ServerDumper('tcp://127.0.0.1:9912', $fallbackDumper, [
+    'cli' => new CliContextProvider(),
+    'source' => new SourceContextProvider(),
+]);
+
+// Set the handler to the ServerDumper class
+VarDumper::setHandler(function ($var) use ($cloner, $dumper) {
+    $dumper->dump($cloner->cloneVar($var));
+});
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+require_once __DIR__ . '/bootstrap.php';
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Wiringa_DumpServer', __DIR__);
